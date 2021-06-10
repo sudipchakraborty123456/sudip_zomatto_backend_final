0eb08e67b7a0898784d9b555fb0dacf0d670e985
sudipchakraborty123456 <sudipchakrabort47@gmail.com>
2021-06-10 23:08:22 +0530

    ff

--- controllers/user.js
+++ controllers/user.js
@@ -2,68 +2,64 @@
 const user = require("../Models/user");
 
 
-exports.singUp =(req,res) => {
+exports.singUp = (req, res) => {
     const reqBody = req.body;
 
-//console.log(reqBody);
-//(2)  call a save mathod
+    //console.log(reqBody);
+    //(2)  call a save mathod
 
-//            a.    we will create an object
+    //            a.    we will create an object
 
-    const {email, password, firstName, lastName} = reqBody;
-   // console.log(email,password,firstName,lastName);
+    const { email, password, firstName, lastName } = reqBody;
+    // console.log(email,password,firstName,lastName);
     const userObj = new user(
         {
-            email : email,
-            password : password,
-            firstName : firstName,
-            lastName : lastName
+            email: email,
+            password: password,
+            firstName: firstName,
+            lastName: lastName
         }
     );
 
     user.find({
-        
-        email : email
-    }).then( data => {
-        if(data.length>0){
-            res.json({"data":"Use another email"})
-        return;
+        email: email
+    }).then(data => {
+        if (data.length > 0) {
+            res.json({ "data": "Use another email" })
+            
         }
-       
-        
-    }).catch(err => { 
-        res.json({ Error :err});
-    })
-    //   b.      call the save method, insert data into MongoDB
-
+    }).catch(err => {
         userObj.save().then(data => {
             res.json({
-                message : "user regesterd successfully",
-                user  : data
+                message: "user regesterd successfully",
+                user: data
             })
         }).catch(err => {
-            res.json({error :err});
+            res.json({ error: err });
         });
-
+    })
+    //   b.      call the save method, insert data into MongoDB
 };
-exports.logIn =(req,res) => {
+exports.logIn = (req, res) => {
     const bodyParser = req.body;
 
-    const { email, password} = bodyParser;
-   // console.log(email,password);
+    const { email, password } = bodyParser;
+    // console.log(email,password);
     user.find({
-        email : email,
-        password : password
-    }).then( data => {
-        if(data.length>0){
-        res.json({message : 'User login successfully',
-        user : data});
+        email: email,
+        password: password
+    }).then(data => {
+        if (data.length > 0) {
+            res.json({
+                message: 'User login successfully',
+                user: data
+            });
         }
-        else{
+        else {
             res.send("UserName or Password is wrong!");
         }
-        
-    }).catch(err => { 
-        res.json({ Error :err});
+
+    }).catch(err => {
+        res.json({ Error: err });
     })
 };
